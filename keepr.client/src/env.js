326eff2dd37cab700f62ff78f72c6e1647bae96d
ShereneC <spyhopper@cableone.net>
2021-09-10 11:40:31 -0600

    env app set OK

--- keepr.client/src/env.js
+++ keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const domain = 'sc-codeworks.us.auth0.com'
 export const audience = 'https://keeprSC.com'
 export const clientId = 'n3o13WtGdGn49mzUqS4cqqozClY9VkR9'
