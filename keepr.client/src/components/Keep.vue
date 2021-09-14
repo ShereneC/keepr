b6aa8ef4bb89abe8b4e94128bedcbeae683c7a7c
ShereneC <spyhopper@cableone.net>
2021-09-13 19:33:31 -0600

    Keep card columns working

--- keepr.client/src/components/Keep.vue
+++ keepr.client/src/components/Keep.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="keep card rounded elevation-1 bg-light my-3 p-3 text-left selectable">
-    {{ keep.creator.picture }}
+  <div class="keep card img-fluid rounded elevation-1 bg-light my-3 p-3 text-left selectable">
+    <img class="card-img-top" :src="keep.img" alt="Card image" style="width:100%">
+    <div class="card-img-overlay d-flex">
+      <h4 class="card-title text-light titleText mt-auto">
+        {{ keep.name }}
+      </h4>
+      <img :src="keep.creator.picture" alt="profile image" class="profile-pic mt-auto ml-auto">
+    </div>
   </div>
 </template>
 
@@ -20,5 +26,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.titleText {
+  text-shadow: -2px -2px 0 #000;
+  // REVIEW  text-shadow: 2px 2px -2 #000; - not happy about this shadow, it will not outline all around the textand it won't let me put it on twice - only takes the first.
+}
 
+.profile-pic{
+  height: 3rem;
+  // width: 5vw;
+  border-radius: 50%
+  // object-fit: cover;
+}
 </style>
